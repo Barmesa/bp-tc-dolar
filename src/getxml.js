cfa8ac9a19c8c55116ac7afbcd8d92ea2b713884
Gustavo de León <alfonso.gus.deleon@gmail.com>
2026-08-20 17:16:18 -0600

    Enhance fetch request for XML data
    Updated fetch request to include method, redirect, and headers.

--- src/getxml.js
+++ src/getxml.js
@@ -8,7 +8,13 @@ export async function getxml() {
 		// y configuramos el tiempo de vida en caché de la respuesta a 1 hora
 		const inicio = Date.now();
 		console.log("Iniciando fetch XML... " + inicio);
-		const xml = await fetch('https://www.dof.gob.mx/indicadores.xml');
+		const xml = await fetch('https://www.dof.gob.mx/indicadores.xml', {
+			method: 'GET',
+			redirect: 'follow', // Fuerza al subrequest de Cloudflare a seguir las redirecciones en el origen
+			headers: {
+				'Accept': 'application/xml, text/xml'
+			}
+		});
 		const fin = Date.now();
 		console.log("Fetch XML completado... " + fin);
 		if (!xml.ok) {
